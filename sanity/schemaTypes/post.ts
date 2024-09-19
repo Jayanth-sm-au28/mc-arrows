6a78c5e548651d6401a4f1a488cc87e4c1a4bed1
Jayanthoutnondgenie <jayanth@outbondgenie.com>
2024-09-20 02:53:22 +0530

    post schema & author schema is created sucessfully, validation ,hotspot, rich text  is also added,

--- sanity/schemaTypes/post.ts
+++ sanity/schemaTypes/post.ts
@@ -1,15 +1,56 @@
+// import { validation } from "sanity";
+import { defineType, defineField, defineArrayMember } from "sanity";
 
-export const post = ({
+export const post = defineType({
   name: "post",
   type: "document",
   title: "Post",
   fields: [
-   {
+    defineField({
+      name: "image",
+      type: "image",
+      title: "Image",
+    }),
+    defineField({
       name: "title",
       type: "string",
       title: "Post Title",
       description: "Title of the post",
-    },
+      validation: (Rule) => Rule.required(),
+    }),
 
+    defineField({
+      name: "slug",
+      type: "slug",
+      title: "Slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      description: "Add your Content",
+      validation: (Rule) => Rule.required(),
+      of: [
+        defineArrayMember({
+          type: "block",
+        }),
+      ],
+    },
+    defineField({
+      name: "author",
+      type: "reference",
+      title: "Author",
+      to: [
+        {
+          type: "author",
+        },
+      ],
+    }),
   ],
 });
