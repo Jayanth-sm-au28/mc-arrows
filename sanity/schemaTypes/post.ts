ff6a187fe2cd23b869fd9ad2dc6fb177a34aa25b
Jayanthoutnondgenie <jayanth@outbondgenie.com>
2024-09-20 01:05:06 +0530

    post schema added and tested sucessfully with sanity content lake,

---
+++ sanity/schemaTypes/post.ts
@@ -0,0 +1,15 @@
+
+export const post = ({
+  name: "post",
+  type: "document",
+  title: "Post",
+  fields: [
+   {
+      name: "title",
+      type: "string",
+      title: "Post Title",
+      description: "Title of the post",
+    },
+
+  ],
+});
